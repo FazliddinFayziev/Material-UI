50dda430ae09411da83ee18176ecda9d9696f0df
FazliddinFayziev <fazrez4515@gmail.com>
2022-12-25 05:55:34 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
+import { PhotoCamera } from '@material-ui/icons';
+
+import useStyles from './style';
+
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 
 function App() {
+  // styling
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position='relative'>
+        <PhotoCamera className={classes.icon} />
+        <Typography variant='h6'>
+          Photo Album
+        </Typography>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+              Photo Album
+            </Typography>
+            <Typography variant='h5' align='center' color='textSecondary' paragraph>
+              Hello everyone This is a photo album. I making it with help of Material UI. I think it is going to be the good practice for myself
+            </Typography>
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant='contained' color='primary'>
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant='outlined' color='primary'>
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {/* mapping */}
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant='h5'>
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use it
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size='small' color='primary'>
+                      View
+                    </Button>
+                    <Button size='small' color='primary'>
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant='h6' align='center' gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant='subtitle1' align='center' color='textSecondary'>
+          Something here for our footer
+        </Typography>
+      </footer>
+    </>
   );
 }
 
